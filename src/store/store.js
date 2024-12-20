5be47805ddae885f7cd6a4aafa0fa44f1f76d73d
sejda1 <sejdaozgur@gmail.com>
2024-12-20 22:10:32 +0300

    redux-logger eklendi

--- src/store/store.js
+++ src/store/store.js
@@ -2,5 +2,5 @@ import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import reducer from './reducers/index.js';
 import logger from 'redux-logger';
 
-export const myStore = createStore(reducer, applyMiddleware(logger));
+export const myStore = createStore(reducer, applyMiddleware(logger)); 
 
